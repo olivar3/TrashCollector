0fd441fd341f1ff7480fe1387a0ea9627a3f6fbe
olivar3 <olivar3@outlook.com>
2017-09-13 10:03:17 -0500

    Added a Mycollection class to models. added base for my table for pickups

---
+++ TrashCollector/TrashCollector/Models/MyCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class MyCollection
+    {
+        public int Id { get; set; }
+        public int NumberThisMonth  { get; set; }
+        public string Date { get; set; }
+        public double Cost { get; set; }
+    }
+}
